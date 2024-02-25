7f9744d16fbbc93cb4be04e1aa01ae00847701ca
Arun Sathyan <arunsathyan660@gmail.com>
2024-02-25 15:28:03 +0530

    Added a new note in Format_Printing file for reference

--- Format_Printing.py
+++ Format_Printing.py
@@ -4,3 +4,14 @@ b=2
 print('The value of a is',a,'& the value of b is',b)
 
 print('The value of a is %d & the value of b is %d'%(a,b))
+
+x='Arun'
+y='Saranya'
+
+print('%s and %s are loving each other since 2019'%(x,y))
+
+""" Just a note
+%d for numbers
+%f for float
+%s for string  
+"""
